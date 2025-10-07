0080864df6ad4c3876d87cb973cf684b26604189
Aleksandar <aleksandar@Aleksandars-MacBook-Pro.local>
2025-10-07 21:43:04 +0200

    Added a comment

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -11,12 +11,14 @@ while True:
     except ValueError:
         print("Please enter numbers only.")
         continue
+
     password = (
         ''.join(secrets.choice(string.ascii_letters) for _ in range(letters)) +
         ''.join(secrets.choice(string.punctuation) for _ in range(symbols)) +
         ''.join(secrets.choice(string.digits) for _ in range(numbers))
     )
 
+    # Shuffle to randomize the order of characters
     password = ''.join(secrets.SystemRandom().sample(password, len(password)))
 
     print(f"Your generated password is:{password}")
